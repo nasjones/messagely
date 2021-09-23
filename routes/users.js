fe841ed5ef81927cc9469c9a8b26e6f07389c209
nasjones <46979749+nasjones@users.noreply.github.com>
2021-09-23 13:36:33 -0400

    user route addition

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const User = require("../models/user");
 const router = express.Router();
+const ExpressError = require("../expressError");
 
 /** GET / - get list of users.
  *
